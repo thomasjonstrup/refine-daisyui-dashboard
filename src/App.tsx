5df043d3f7a4375d3e7a5a2eddca1526d6222f5a
Thomas Jonstrup <thomas_jonstrup@hotmail.com>
2024-03-30 17:15:44 +0100

    Initial commit from create-refine-app

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import {
+  Refine,
+  GitHubBanner,
+  WelcomePage,
+  Authenticated,
+  AuthPage,
+  ErrorComponent,
+} from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import dataProvider from "@refinedev/simple-rest";
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
+import routerBindings, {
+  NavigateToResource,
+  CatchAllNavigate,
+  UnsavedChangesNotifier,
+  DocumentTitleHandler,
+} from "@refinedev/react-router-v6";
+import { Layout } from "./components/layout";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <DevtoolsProvider>
+          <Refine
+            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+            routerProvider={routerBindings}
+            options={{
+              syncWithLocation: true,
+              warnWhenUnsavedChanges: true,
+              useNewQueryKeys: true,
+              projectId: "SvWsMS-NJj8Dy-On3PHl",
+            }}
+          >
+            <Routes>
+              <Route index element={<WelcomePage />} />
+            </Routes>
+            <RefineKbar />
+            <UnsavedChangesNotifier />
+            <DocumentTitleHandler />
+          </Refine>
+          <DevtoolsPanel />
+        </DevtoolsProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
